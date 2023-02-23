939e0304be760d2ffe0b74193a76f637ec3aa184
Roberto <Robertoaraujo230@hotmail.com>
2023-02-22 22:18:34 -0300

    Refatorando a função SmoothScroll para uma classe

--- assets/js/modules/initSmoothScroll.js
+++ assets/js/modules/initSmoothScroll.js
@@ -1,18 +1,29 @@
-export default function initSmoothScroll() {
-  const internalLinks = document.querySelectorAll('a[href^="#"]');
+export default class SmoothScroll {
+  constructor(link, options) {
+    this.internalLinks = document.querySelectorAll(link);
+    if (options === undefined) {
+      this.options = { behavior: "smooth", block: "start" };
+    } else this.options = options;
 
-  function scrollToSection(event) {
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
+
+  scrollToSection(event) {
+    console.log(this);
     event.preventDefault();
     const href = event.currentTarget.getAttribute("href");
     const section = document.querySelector(href);
+    section.scrollIntoView(this.options);
+  }
 
-    section.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
+  addLinkEvent() {
+    this.internalLinks.forEach((link) => {
+      link.addEventListener("click", this.scrollToSection);
     });
   }
 
-  internalLinks.forEach((link) => {
-    link.addEventListener("click", scrollToSection);
-  });
+  init() {
+    if (this.internalLinks.length) this.addLinkEvent();
+    return this;
+  }
 }
