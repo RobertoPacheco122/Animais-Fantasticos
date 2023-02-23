ab95c595e137529a4141f9ab1bffcd2e5b4c62d6
Roberto <Robertoaraujo230@hotmail.com>
2023-02-22 22:58:32 -0300

    Instanciando e iniciando a classe Accordion em script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-import initTabNav from "./modules/initTabNav.js";
-import initAccordionList from "./modules/initAccordionList.js";
 import SmoothScroll from "./modules/initSmoothScroll.js";
+import AccordionList from "./modules/initAccordionList.js";
+import initTabNav from "./modules/initTabNav.js";
 import initAnimationScroll from "./modules/initAnimationScroll.js";
 import initModal from "./modules/initiModal.js";
 import initTooltip from "./modules/initToolTip.js";
@@ -9,11 +9,13 @@ import initOpeningSchedule from "./modules/initOpeningSchedule.js";
 import initAnimalsFetch from "./modules/initAnimalsFetch.js";
 import initBitcoinFetch from "./modules/initBitcoinFetch.js";
 
-initTabNav();
-initAccordionList();
 const smoothScroll = new SmoothScroll('a[href^="#"]');
 smoothScroll.init();
 
+const accordionList = new AccordionList('[data-anime="accordion"] dt');
+accordionList.init();
+
+initTabNav();
 initAnimationScroll();
 initModal();
 initTooltip();
