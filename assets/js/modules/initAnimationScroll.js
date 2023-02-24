d15ffeea8e4666860e7f6c431e10bf6a85d7e052
Roberto <Robertoaraujo230@hotmail.com>
2023-02-23 23:15:59 -0300

    Refazendo o initAnimationScroll com classe

--- assets/js/modules/initAnimationScroll.js
+++ assets/js/modules/initAnimationScroll.js
@@ -1,19 +1,35 @@
-export default function initAnimationScroll() {
-  function animateScroll() {
-    const sections = document.querySelectorAll('[data-anime="scroll"]');
+export default class AnimationScroll {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.halfWindowHeight = window.innerHeight * 0.6;
 
-    sections.forEach((section) => {
-      const heightToTop = section.getBoundingClientRect().top;
-      const halfWindowHeight = window.innerHeight * 0.6;
-      const heightToShow = heightToTop - halfWindowHeight < 0;
+    this.checkDistance = this.checkDistance.bind(this);
+  }
 
-      if (heightToShow) {
-        section.classList.add("ativo");
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset) - this.halfWindowHeight,
+      };
+    });
+  }
+
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.scrollY > item.offset) {
+        item.element.classList.add("ativo");
       }
     });
   }
 
-  animateScroll();
-
-  window.addEventListener("scroll", animateScroll);
+  init() {
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener("scroll", this.checkDistance);
+    }
+    return this;
+  }
 }
